be7ce38a18b290db6f1a7463b85ffbb034ba313b
alexziskind1 <alex@nuvious.com>
2021-05-06 09:56:31 -0400

    advanced complete

--- RPS.Web.Server/Controllers/ReportsController.cs
+++ RPS.Web.Server/Controllers/ReportsController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Telerik.Reporting.Processing;
 using Telerik.Reporting.Services;
 using Telerik.Reporting.Services.AspNetCore;
 
@@ -16,5 +17,16 @@ namespace RPS.Web.Server.Controllers
         public ReportsController(IReportServiceConfiguration reportServiceConfiguration)
             : base(reportServiceConfiguration)
         { }
+
+        protected override UserIdentity GetUserIdentity()
+        {
+            var identity = base.GetUserIdentity();
+            identity.Context = new System.Collections.Concurrent.ConcurrentDictionary<string, object>();
+            // identity.Context["UrlReferrer"] = System.Web.HttpContext.Current.Request.UrlReferrer;
+
+            // Any other available information can be stored in the identity.Context in the same way
+
+            return identity;
+        }
     }
 }
